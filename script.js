7848e8e40b52d27f12161e6d666e30bdfd602652
Bruno-fmaciel <bfma@cesar.school>
2024-02-25 16:45:41 -0300

    add slider (create grid not working)

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const container = document.querySelector('.container');
-function createGrid(row,collumn) {
-    gridArea = row * collumn
+const sliderContainer = document.querySelector('.slider');
+const slider = document.querySelector('.slider');
+const sliderValue = document.querySelector('.slider-value');
+let squaresNumber = 16;
+
+sliderValue.textContent = `Resolution: ${slider.value} x ${slider.value}`;
+
+function createGrid(squaresNumber) {
+    gridArea = squaresNumber * squaresNumber;
     for (i = 0;i <= gridArea-1;i++) {
         const div = document.createElement("div");
-        div.style.width = `${(800/collumn)}px`;
-        div.style.height = `${(800/collumn)}px`;
+        div.style.width = div.style.height = `${(800/squaresNumber)}px`;
         div.classList.add('square');
         container.appendChild(div);
     }
 }
 
-createGrid(16,16);
+createGrid(squaresNumber);
+
 
 const squares = document.querySelectorAll('.square');
 squares.forEach(function(square) {
@@ -24,6 +31,20 @@ reset.addEventListener('click', function(e){
     squares.forEach(function(square) {
         square.style.background = 'white';
     })
+})
+
+function removeSquares() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }    
+}
+
+
+slider.addEventListener('input',function() {
+    let text = `Resolution: ${slider.value} x ${slider.value}`;
+    sliderValue.innerHTML = text;
+    removeSquares();
+    createGrid(slider.value);
 })
 
 
